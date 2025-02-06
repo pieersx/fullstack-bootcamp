a727ca13cfaad88a66ff4ad8b976cc6875f8ac42
Pieers <piers_84@hotmail.com>
2025-02-06 18:14:11 -0500

    Exercises 2.11 - phonebook

--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -3,7 +3,11 @@ import { Person } from './Person'
 export const Persons = ({ persons }) => (
   <ul>
     {persons.map((person) => (
-      <Person key={person.id} name={person.name} number={person.number} />
+      <Person
+        key={person.id}
+        name={person.name}
+        number={person.number}
+      />
     ))}
   </ul>
 )
